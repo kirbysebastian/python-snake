dca8accce3d94a337bae434187697256acabb83d
kirbysebastian <kirbypaolo.sebastian@gmail.com>
2020-03-29 17:30:59 +0800

    implement out of bounce

--- game/snake_game.py
+++ game/snake_game.py
@@ -8,7 +8,6 @@ class SnakeGame:
 
     def __init__(self):
         self.snake = Snake()
-        #self.snake_pos = (self.game_height//2, self.game_width//2)
         self.game_over = False
         self.game_gui = pygame
 
@@ -26,6 +25,7 @@ class SnakeGame:
         while not self.game_over:
             self.getEvents()
             self.getInput()
+            self.updateGame()
             self.draw()
 
     def getEvents(self):
@@ -34,6 +34,27 @@ class SnakeGame:
                 self.game_over = True
                 self.game_gui.quit()
 
+    def updateGame(self):
+        new_x_coord = self.snake.x_coord + self.snake.x_speed
+        new_y_coord = self.snake.y_coord + self.snake.y_speed
+
+        if self.out_of_bounce(new_x_coord, new_y_coord):
+            self.game_over = True
+
+        self.updateSnake(new_x_coord, new_y_coord)
+        #print("X: ", self.snake.x_speed)
+        #print("Y: ", self.snake.y_speed)
+
+    def out_of_bounce(self, x_coord, y_coord):
+        if (y_coord+10 > self.game_height
+            or x_coord+10 > self.game_width
+            or y_coord <= 0
+            or x_coord <= 0):
+            print("NADA")
+            return True
+
+        return False
+           
     def getInput(self):
        # To modify with different types of INPUT
        key = self.game_gui.key.get_pressed()
@@ -52,26 +73,12 @@ class SnakeGame:
            self.snake.x_speed = 0
            self.snake.y_speed = self.snake.default_speed
 
-       self.updateSnake()
-       #print("X: ", self.snake.x_speed)
-       #print("Y: ", self.snake.y_speed)
+    def updateSnake(self, x_coord, y_coord):
+        print(x_coord)
+        print(y_coord)
 
-
-    def updateSnake(self):
-        new_x_coord = self.snake.x_coord + self.snake.x_speed
-        new_y_coord = self.snake.y_coord + self.snake.y_speed
-
-        if (new_y_coord+10 > self.game_height
-            or new_x_coord+10 > self.game_width
-            or new_y_coord <= 0
-            or new_x_coord <= 0):
-            print("NADA")
-            return
-           
-        print(new_x_coord)
-        print(new_y_coord)
-        self.snake.x_coord = new_x_coord
-        self.snake.y_coord = new_y_coord
+        self.snake.x_coord = x_coord
+        self.snake.y_coord = y_coord
 
     def clearScreen(self):
         self.screen.fill((0, 0, 0))
